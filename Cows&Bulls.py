b762dde00339e4aa24c93a71a1bbadfa69439a98
blue5had0w <45438121+blue5had0w@users.noreply.github.com>
2019-07-02 22:35:03 +0300

    Update Cows&Bulls.py

--- Cows&Bulls.py
+++ Cows&Bulls.py
@@ -1,34 +1,35 @@
 import random
 
 print("Welcome to the Cows and Bulls Game!")
+username = input("Please, enter your name: ")
 
 comp_num = "".join(str(i) for i in random.sample(range(0, 10), k=4))
 
-'''
-comp_num = ""
-while len(comp_num) <= 3:
-    x = random.randint(0, 9)
-    y = str(x)
-    if y not in comp_num:
-        comp_num += y
-'''
-#print(comp_num)
+print(comp_num)
 
-def checking(user_guess, comp_num):
+def checking(user_guess, comp_num, user):
     attempts = 1
     while True:
         if user_guess.lower().startswith("e"):
-            print("LOOSER !!!\n\nYou have made", attempts - 1, "attempts.")
+          if attempts == 2:
+            print("\nLOOSER !!! Ha - Ha \n\n{}, you have made {} attempt.".format(user, attempts - 1))
             break
+          elif attempts == 1:
+              print("\nWoow!!! {}, you are such a quiter. SHAME! You have made {} attempts.".format(user, attempts - 1))
+              break
+          else:
+              print("\nLOOSER !!! Ha - Ha \n\nYou have made", attempts - 1, "attempts.")
+              break
         else:
             bulls = 0
             cows = 0
-            if len(user_guess) > 4:
+            if user_guess.isalpha():
+                print("Hey, {}! Only digits!".format(user))
+            elif len(user_guess) > 4:
                 print("Your number must be exact 4 digits!")
             elif len(user_guess) < 4:
                 print("Your number must be exact 4 digits!")
             else:
-
                 if user_guess[0] == comp_num[0]:
                     bulls += 1
                 if user_guess[1] == comp_num[1]:
@@ -37,7 +38,6 @@ def checking(user_guess, comp_num):
                     bulls += 1
                 if user_guess[3] == comp_num[3]:
                     bulls += 1
-
                 if user_guess[0] in comp_num:
                     if user_guess[0] not in comp_num[0]:
                         cows += 1
@@ -71,7 +71,6 @@ def checking(user_guess, comp_num):
                 attempts += 1
             user_guess = input("Guess the number. Enter a four digit number or type Exit to quit: ")
 
-
 if __name__ == "__main__":
     user_guess = input("Guess the number. Enter a four digit number or type Exit to quit: ")
-    print(checking(user_guess, comp_num))
+print(checking(user_guess, comp_num, username))
